ac52f0b19b5451df8a4a5257580313fd5d14e184
Edward Birchall <s2077814@ed.ac.uk>
2024-03-26 11:45:16 +0000

    add setup script

---
+++ EJDB-setup.sh
@@ -0,0 +1,26 @@
+# TODO only install things if not installed
+
+sudo apt update
+sudo apt install git
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# TODO download .tmux.conf
+
+cd ~/
+curl https://github.com/birchtree02/SETUP/raw/main/.tmux.conf
+
+sudo apt install zsh
+
+# TODO use wget if no curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir ~/.vim
+mkdir ~/.vim/pack
+mkdir ~/.vim/pack/plugin
+mkdir ~/.vim/pack/plugin/start
+
+cd ~/.vim/pack/plugin/start
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+
+cd ~/
